f51e697ad3200a6dbfa7d1f56d0a5be80817c27e
Fredrik <91frerin@gafe.molndal.se>
2024-03-14 09:38:39 +0100

    lade till httpclient-dependency

--- src/main/java/fredrikkodar/service/UserService.java
+++ src/main/java/fredrikkodar/service/UserService.java
@@ -1,4 +1,10 @@
 package fredrikkodar.service;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
 public class UserService {
+
+    private static final CloseableHttpClient httpClient = HttpClients.createDefault();
+
 }
