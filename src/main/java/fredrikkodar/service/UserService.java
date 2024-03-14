ed3cab7f04579833f248eca84a6212bb00ba0066
Fredrik <91frerin@gafe.molndal.se>
2024-03-14 11:04:06 +0100

    changePassword och deleteUser

--- src/main/java/fredrikkodar/service/UserService.java
+++ src/main/java/fredrikkodar/service/UserService.java
@@ -8,6 +8,7 @@ import fredrikkodar.model.LoginResponse;
 import fredrikkodar.model.User;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.classic.methods.HttpPut;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -48,7 +49,8 @@ public class UserService {
 
                 ObjectMapper mapper = new ObjectMapper();
 
-                User responseUser = mapper.readValue(EntityUtils.toString(payload), new TypeReference<User>() {});
+                User responseUser = mapper.readValue(EntityUtils.toString(payload), new TypeReference<User>() {
+                });
 
                 System.out.printf("User %s has been created with the user-id: %d%n", responseUser.getUsername(), responseUser.getId());
 
@@ -65,7 +67,7 @@ public class UserService {
         }
     }
 
-    public static LoginResponse login(){
+    public static LoginResponse login() {
 
         try {
             String username = getStringInput("Enter username ");
@@ -86,7 +88,8 @@ public class UserService {
                 HttpEntity payload = response.getEntity();
 
                 ObjectMapper mapper = new ObjectMapper();
-                LoginResponse loginResponse = mapper.readValue(EntityUtils.toString(payload), new TypeReference<LoginResponse>() {});
+                LoginResponse loginResponse = mapper.readValue(EntityUtils.toString(payload), new TypeReference<LoginResponse>() {
+                });
                 if (loginResponse.getUser() == null) {
                     System.out.println("Wrong username or password. Please try again.");
                     return null;
@@ -101,14 +104,16 @@ public class UserService {
                 System.out.println("IO Exception Error: " + e.getMessage());
             } catch (ParseException e) {
                 System.out.println("Parse Error: " + e.getMessage());
-            } return null;
+            }
+            return null;
 
         } catch (Exception e) {
             System.out.println("Something went wrong: " + e.getMessage());
-        } return null;
+        }
+        return null;
     }
 
-    public static List<User> getUsers(String jwt) {
+    public static List<User> getAllUsers(String jwt) {
 
         try {
             HttpGet request = new HttpGet("http://localhost:8081/users/all");
@@ -125,7 +130,8 @@ public class UserService {
 
                 ObjectMapper mapper = new ObjectMapper();
 
-                return mapper.readValue(EntityUtils.toString(entity), new TypeReference<ArrayList<User>>() {});
+                return mapper.readValue(EntityUtils.toString(entity), new TypeReference<ArrayList<User>>() {
+                });
 
             } catch (JsonMappingException e) {
                 System.out.println("Json Mapping Error: " + e.getMessage());
@@ -138,7 +144,51 @@ public class UserService {
 
         } catch (Exception e) {
             System.out.println("Something went wrong: " + e.getMessage());
-        } return null;
+        }
+        return null;
+    }
+    public static void deleteAccount(Long id, String jwt) {
+        try {
+            HttpPost request = new HttpPost("http://localhost:8081/users/me/");
+            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+
+                if (response.getCode() != 200) {
+                    System.out.println("Something went wrong with the request: " + response.getCode());
+                    return;
+                }
+                System.out.println("User deleted successfully");
+
+            } catch (JsonProcessingException e) {
+                System.out.println("Json Processing Error: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("IO Error: " + e.getMessage());
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
     }
 
+    public static void changePassword (String jwt, String oldPassword, String newPassword, String confirmPassword) {
+        try {
+            ChangingPassword changingPassword = new ChangingPassword(oldPassword, newPassword, confirmPassword);
+            HttpPut request = new HttpPut("http://localhost:8081/users/changePass/");
+            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
+            request.setEntity(createPayload(changingPassword));
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                if (response.getCode() != 200) {
+                    System.out.println("Something went wrong with the request: " + response.getCode());
+                    return;
+                }
+                System.out.println("Password changed successfully");
+            } catch (JsonProcessingException e) {
+                System.out.println("Json Processing Error: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("IO Error: " + e.getMessage());
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
 }
