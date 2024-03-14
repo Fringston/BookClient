04e5c41fe193c0a10998cc0cf1b6c20f64fb2bf3
Fredrik <91frerin@gafe.molndal.se>
2024-03-14 10:12:03 +0100

    UserService och UtilService

--- src/main/java/fredrikkodar/model/User.java
+++ src/main/java/fredrikkodar/model/User.java
@@ -3,13 +3,17 @@ package fredrikkodar.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class User {
     private Long id;
     private String username;
     private String password;
     private Role role;
+
+    public User(Long l, String username, String password) {
+    }
 }
