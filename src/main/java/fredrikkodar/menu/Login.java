94c7d83210d5d7a701d8b931428c32b98ded621d
clarabrorson <88clabro@gafe.molndal.se>
2024-03-14 10:13:24 +0100

    UserMenu.java

--- src/main/java/fredrikkodar/menu/Login.java
+++ src/main/java/fredrikkodar/menu/Login.java
@@ -1,4 +1,52 @@
 package fredrikkodar.menu;
 
+import fredrikkodar.model.LoginResponse;
+import fredrikkodar.service.UserService;
+import fredrikkodar.service.UtilService;
+import org.apache.hc.core5.http.ParseException;
+
+import java.io.IOException;
+
 public class Login {
+
+    private boolean isRunning = true;
+
+    public void loginChoice() {
+        String[] loginOptions = {"1. Login", "2. Back to Main Menu\n"};
+        for (String option : loginOptions) {
+            System.out.println(option);
+        }
+    }
+
+    public void runLoginMenu() throws IOException, ParseException {
+        while (isRunning) {
+            loginChoice();
+            int choice = UtilService.getIntInput("Enter choice: ");
+            userChoice(choice);
+        }
+    }
+
+    public void userChoice(int choice) throws IOException, ParseException {
+        switch (choice) {
+            case 1:
+                LoginResponse loginResponse = UserService.login();
+                if (loginResponse != null) {
+                    System.out.println("Login successful!");
+                    System.out.println("Welcome " + loginResponse.getUser().getUsername());
+                    UserMenu userMenu = new UserMenu();
+                    userMenu.runUserMenu(loginResponse.getJwt());
+                } else {
+                    System.out.println("Login failed. Please try again.");
+                }
+
+            case 2:
+                MainMenu mainMenu = new MainMenu();
+
+        }
+
+    }
 }
+
+
+
+
--- src/main/java/fredrikkodar/menu/UserMenu.java
+++ src/main/java/fredrikkodar/menu/UserMenu.java
@@ -1,4 +1,46 @@
 package fredrikkodar.menu;
 
+import fredrikkodar.service.UtilService;
+
 public class UserMenu {
+
+    private boolean isRunning = true;
+
+
+    private boolean isRunning = true;
+
+    public void userMenuChoice() {
+        String[] userMenuOptions = {"1. Library", "2. Handle account", "3. Logout\n"};
+        for (String option : userMenuOptions) {
+            System.out.println(option);
+        }
+    }
+
+    public void runUserMenu(String jwt) {
+        while (isRunning) {
+            userMenuChoice();
+            int choice = UtilService.getIntInput("Enter choice: ");
+            userMenuChoice(choice, jwt);
+        }
+    }
+
+    private void userMenuChoice(int choice, String jwt) {
+        switch (choice) {
+            case 1:
+                System.out.println("Library\n");
+
+                break;
+            case 2:
+                System.out.println("Handle account\n");
+                AccountHandler accountHandler = new AccountHandler();
+                accountHandler.runAccountMenu();
+                break;
+            case 3:
+                AdminMenu adminMenu = new AdminMenu();
+                adminMenu.runAdminMenu(jwt);
+                break;
+            default:
+                System.out.println("Invalid choice\n");
+        }
+    }
 }
