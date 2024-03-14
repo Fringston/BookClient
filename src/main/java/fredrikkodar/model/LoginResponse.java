c78e7b676f292874c25590ad7eeadf544d06aff4
clarabrorson <88clabro@gafe.molndal.se>
2024-03-14 08:41:06 +0100

    added Author + LoginResponse

--- src/main/java/fredrikkodar/model/LoginResponse.java
+++ src/main/java/fredrikkodar/model/LoginResponse.java
@@ -1,5 +1,16 @@
 package fredrikkodar.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class LoginResponse {
+
+    private User user;
+    private String jwt;
 
 }
